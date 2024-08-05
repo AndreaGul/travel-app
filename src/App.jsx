e266d7bd4e1f655dafcd386145bf5dd5cc7bd773
AndreaGul <andreagulino99@gmail.com>
2024-08-05 17:29:03 +0200

    feat: off canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import  Offcanvas  from 'react-bootstrap/Offcanvas';
 
 function App() {
   const [showOffCanvas, setShowOffCanvas] = useState(false);
@@ -14,7 +14,7 @@ function App() {
         <div className='container-travel d-flex justify-content-between align-items-center'>
           <h1 className='fw-semibold'>Pianifica il tuo viaggio</h1>
           <div>
-            <button onClick={handleShowOffCanvas} className='btn-meta'>
+            <button onClick={handleShowOffCanvas} className='btn-default btn-meta'>
               Aggiungi meta
             </button>
 
@@ -22,28 +22,30 @@ function App() {
          
           <Offcanvas className="offcanvas-container" show={showOffCanvas} onHide={handleCloseOffCanvas} placement='end'>
             <Offcanvas.Header className='offcanvas-header p-0' closeButton>
-              <h3 className='p-0'>Aggiungi meta</h3>
+              <h3 className='p-0 m-0'>Aggiungi meta</h3>
 
             </Offcanvas.Header>
             <Offcanvas.Body className='p-0'>
               <div className="form-container global-input-title  global-input">
                 <form action="">
-                  <h4>Luogo</h4>
-                  <input type="text" />
+                  <h5>Titolo</h5>
+                  <input className='w-100' type="text" />
 
-                  <h4>Data</h4>
+                  <h5>Luogo</h5>
+                  <input className='w-100' type="text" />
+                  
+                  <h5>Data</h5>
                   <input type="date" />
 
-                  <h4>Titolo</h4>
-                  <input type="text" />
+                  
+                  <h5>Immagine</h5>
+                  <input type="file"  className='w-100'/>
 
-                  <h4>img</h4>
-                  <input type="file" />
+                 
+                  <h5>Descrizione</h5>
+                  <textarea  rows={4} className='w-100 d-block' name="" id=""></textarea>
 
-                  <h4>Descrizione</h4>
-                  <textarea name="" id=""></textarea>
-
-                  <button>invia</button>
+                  <button className='btn-default btn-form'>Aggiungi</button>
                 </form>
               </div>
             </Offcanvas.Body>
