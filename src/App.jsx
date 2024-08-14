f6c31e80d122f8282de86fc735a2246a4300efd4
AndreaGul <andreagulino99@gmail.com>
2024-08-14 17:45:55 +0200

    feat:aggiunta di un elemento al submit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,40 @@ function App() {
   const handleCloseOffCanvas = () => setShowOffCanvas(false);
   const handleShowOffCanvas = () => setShowOffCanvas(true);
 
+
+  const [formData, setFormData] = useState({
+    titolo:'',
+    luogo:'',
+    data:'',
+    immagine: null,
+    descrizione: '',
+  })
+
+  const [items, setItems] = useState([]);
+
+  const handleChange = (e)=>{
+    const { name, value, type, files} = e.target;
+    setFormData ({
+      ...formData,
+      [name]: type === 'file' ? files[0]: value,
+    })
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setItems([...items, formData]);
+
+    setFormData({
+      titolo:'',
+      luogo:'',
+      data:'',
+      immagine: null,
+      descrizione: '',
+    });
+  };
+
+  console.log(items);
+
   return (
     <>
       
@@ -22,7 +56,7 @@ function App() {
             </button>
 
           </div>
-         
+          
           <Offcanvas className="offcanvas-container" show={showOffCanvas} onHide={handleCloseOffCanvas} placement='end'>
             <Offcanvas.Header className='offcanvas-header p-0' closeButton>
               <h3 className='p-0 m-0'>Aggiungi meta</h3>
@@ -30,56 +64,56 @@ function App() {
             </Offcanvas.Header>
             <Offcanvas.Body className='p-0'>
               <div className="form-container global-input-title  global-input">
-                <form action="">
+                <form onSubmit={handleSubmit}>
                   <h5>Titolo</h5>
-                  <input className='w-100' type="text" />
+                  <input className='w-100' type="text" name='titolo' value={formData.titolo} onChange={handleChange} />
 
                   <h5>Luogo</h5>
-                  <input className='w-100' type="text" />
+                  <input className='w-100' type="text" name='luogo' value={formData.luogo} onChange={handleChange}/>
                   
                   <h5>Data</h5>
-                  <input type="date" />
+                  <input type="date" name='data' value={formData.data} onChange={handleChange}/>
 
                   
                   <h5>Immagine</h5>
-                  <input type="file"  className='w-100'/>
+                  <input className='w-100' type="file" name='immagine' value={formData.immagine} onChange={handleChange}/>
 
                  
                   <h5>Descrizione</h5>
-                  <textarea  rows={4} className='w-100 d-block' name="" id=""></textarea>
+                  <textarea rows={4} className="w-100 d-block" name="descrizione" value={formData.descrizione} onChange={handleChange}></textarea>
 
-                  <button className='btn-default btn-form'>Aggiungi</button>
+                  <button type='submit' className='btn-default btn-form'>Aggiungi</button>
                 </form>
               </div>
             </Offcanvas.Body>
           </Offcanvas>
         </div>
       
-
         <div className="cal-map-container m-0 mb-5  row g-4">
           <div className="col-4 m-0 ps-0"><div className="Calendar h-100">Calendario</div></div>
           <div className="col-8  m-0 pe-0"><div className="Map h-100">Mappa</div></div>
         </div>
         <div>
         <Accordion className='pb-4'>
-          <Accordion.Item eventKey="0" className='accordion-item'>
+          {items.map((item,index) =>(
+          <Accordion.Item eventKey={index} className='accordion-item'>
             <Accordion.Header className='custon-accordion-header'>  
               <label className="accordion-checkbox-container">
                 <input className='accordion-checkbox' type="checkbox" />
                 <span className="custom-checkbox"></span>
               </label>
-              <h5 className='mb-0'>Titolo</h5>
+              <h5 className='mb-0'>{item.titolo}</h5>
             </Accordion.Header>
             <Accordion.Body  className='d-flex'>
               
               <div className="img-travel-container me-3">
-                <img src="https://store-images.s-microsoft.com/image/apps.30323.14537704372270848.6ecb6038-5426-409a-8660-158d1eb64fb0.08703491-f5dc-4b00-bca6-486b7b293c17?q=90&w=480&h=270" alt="img" className="img-travel"/>
+                <img src={item.immagine}/>
               </div>
               
               
               <div className='d-flex descr-btn-containter '>
                 <div className='Description me-2'>
-                  <p className=" mb-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, voluptas quaerat distinctio itaque consequuntur veritatis voluptatibus et, repellat voluptate consectetur iusto commodi iste obcaecati error eligendi sint amet laudantium porro?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quae culpa aliquam! Cupiditate totam quos, aliquam earum impedit modi quo asperiores officia, aspernatur, tempore nobis animi necessitatibus hic quaerat in!</p>
+                  <p className=" mb-auto">{item.descrizione}</p>
                 </div>
                 <div className='d-flex  flex-column'>
                   <button className="mb-auto acc-default-btn edit-btn"><MdEdit /></button>
@@ -90,6 +124,7 @@ function App() {
               
             </Accordion.Body>
           </Accordion.Item>
+          ))}
           <Accordion.Item eventKey="1" className='accordion-item'>
             <Accordion.Header className='custon-accordion-header'>  
               <label className="accordion-checkbox-container">
